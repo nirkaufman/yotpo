4f2525c24b1aa351ab3b379b7fa3846116f28d8e
nir.k <nir.k@next-insurance.com>
2022-06-06 10:37:02 +0300

    more

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+
+// Helper function to v=create Element (Object)
 function createElement(type, props, ...children) {
   return { type, props, children };
 }
 
+// Component: Function that return an element (object)
+function Button({ text, onClick }) {
+  return createElement('button', {onClick}, text);
+}
+
 const App = createElement('div', null,
   createElement('input', { type: 'text' }),
-  createElement('button', null, 'click'),
+  Button({ text: 'submit', onClick: function () {alert(2)} }),
   createElement('ul', null,
     createElement('li', null, '#1'),
     createElement('li', null, '#2'),
     createElement('li', null, '#3'),
   ),
 )
+
+console.log(App);
 
