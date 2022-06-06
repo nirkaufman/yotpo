cbf8b7cc6a7781a3d6254f47f311429d6c313c2c
nir.k <nir.k@next-insurance.com>
2022-06-06 12:03:03 +0300

    more

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
-import {createElement} from '../libs/react.js';
+import { createElement } from '../libs/react.js';
 
 
 // Component: Function that return an element (object)
-function Button({ onClick, label}) {
-  return createElement('button', {onClick}, label );
+function Button({ onClick, label }) {
+  return createElement('button', { onClick }, label);
 }
 
 const colors = ['red', 'green', 'blue'];
 
-function List() {
-
+function List({ items }) {
+  return (
+    createElement('ul', null,
+      ...items.map( item =>  createElement('li', null, item))
+    )
+  )
 }
 
 // ----------------------------------------------------
@@ -18,13 +22,12 @@ const App = createElement('div', null,
   // <input type={} />
   createElement('input', { type: 'text' }),
   // <Button onClick={}>click</Button>
-  createElement(Button, {label: 'click', onClick: function () {alert(2)} }),
-  createElement(List, {items: colors}),
-  createElement('ul', null,
-    createElement('li', null, '#1'),
-    createElement('li', null, '#2'),
-    createElement('li', null, '#3'),
-  ),
+  createElement(Button, {
+    label: 'click', onClick: function () {
+      alert(2)
+    }
+  }),
+  createElement(List, { items: colors }),
 )
 
 export default App;
